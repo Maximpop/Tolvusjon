df202d3bc5983295285f67784e9a7bbb832547a3
Maximpop <rudkovpop@gmail.com>
2022-11-28 22:22:53 +0000

    A2

--- A2.py
+++ A2.py
@@ -1,43 +1,93 @@
 import cv2
 import time
 import numpy as np
+import random
+
+def ransac(cor,err, threshold=0.3,iter= 50):
+    topScore = 0
+    topLineInliners = None
+    topLine = None
+    thres = len(cor)*threshold
+
+    for i in range(iter): #main for loop
+        #random points 
+        corA = random.choice(cor) 
+        corB = random.choice(cor)
+
+        # line between points calc y = mx + b
+        m = (corA[1]-corB[1])/(corA[0]-corB[0])
+        b = corA[1] - m*corA[0]
+
+        #counter that counts number of points within error value
+        inliner = []
+        for i, (x,y) in enumerate(cor):
+            e = y - (m*x+b)
+            if abs(e) < err: # if abs of e is smaller than our preset error, add to the list
+                inliner.append(i)
+            if len(inliner) > thres:
+                return (m,b), inliner
+            else: 
+                if topScore < len(inliner):
+                    topScore = len(inliner)
+                    topLine = (m,b)
+                    topLineInliners = inliner
+    return topLine,topLineInliners
 
 
 Reddest = False
 brightest_spot = "opencv" #"loops" for two for loops
 
 t_lower = 50  # Lower Threshold
-t_upper = 150  # Upper threshold
+t_upper = 200  # Upper threshold
 
 cap = cv2.VideoCapture(0)
 retur = True
 ti = time.monotonic() # time for frame rate
 retur, frame = cap.read()
+#frame = cv2.imread("pic2.png")
 if not retur:
     print("Camera not loaded")
-frame = cv2.Canny(frame,t_lower,t_upper)    
+frameCan = cv2.Canny(frame,t_lower,t_upper)    
 #print(frame[20,200])
 #print(frame.shape[1])
 #arrray = np.array(frame.shape[0]*frame.shape[1])
-data = []
-for x in range(frame.shape[0]): #Max value search with x and y cordinates
-    for y in range(frame.shape[1]):
-        #print(frame[x,y])
-        if frame[x, y] != 0:
-            data.append([y,x])
-              
-print(data[0][0])
+#A the man the myth the legend Steinarr has shown me a better way! no need for two for loops....
+#data = []
+#for x in range(frame.shape[0]): #Max value search with x and y cordinates
+#    for y in range(frame.shape[1]):
+#        #print(frame[x,y])
+#        if frame[x, y] != 0:
+#            data.append([y,x])
+cor = np.argwhere(frameCan)
+
+
 #image = cv.circle(image, centerOfCircle, radius, color, thickness)
 #cv2.circle(frame,data[0],3,(255, 0, 64),5)
-for i in range(len(data)):
-
-    cv2.circle(frame,data[i],1,(255, 0, 64),1)
+for i in range(len(cor)):
+    #code to lighten up edges
+    #cv2.circle(frame,tuple((int(cor[i][1]), int(cor[i][0]))),1,(255, 0, 64),1)
     pass
-while True:
-      
+line , inliner = ransac(cor,5)
+xcor = 1000*line[0]+line[1]
+xcor = int(xcor)
 
-    
+cv2.imshow("Assigment2", frame)
+cv2.line(frame,(int(line[1]),0),(xcor,1000),(255,255,255),2)
+#cv2.line(frameCan,(a[1],a[0]),(b[1],b[0]),(255,255,255),2)
+while True:
+    retur, frame = cap.read()
+    frameCan = cv2.Canny(frame,t_lower,t_upper)
+    cor = np.argwhere(frameCan)
+    line , inliner = ransac(cor,5)
+    xcor = 1000*line[0]+line[1]
+    xcor = int(xcor)
+    cv2.line(frame,(int(line[1]),0),(xcor,1000),(255,255,255),2)
     cv2.imshow("Assigment2", frame)
+    
+    
+    
+    #cv2.imshow("Assigment2", frameCan)
+
     k = cv2.waitKey(10) & 0xFF
     if k == 27:
         break
@@ -46,6 +96,12 @@ while True:
         print("using two for loop")
     elif k== ord('c'):
         retur, frame = cap.read()
-        frame = cv2.Canny(frame,t_lower,t_upper)
+        frameCan = cv2.Canny(frame,t_lower,t_upper)
+        cor = np.argwhere(frameCan)
+        line , inliner = ransac(cor,5)
+        xcor = 1000*line[0]+line[1]
+        xcor = int(xcor)
+        cv2.line(frameCan,(int(line[1]),0),(xcor,1000),(255,255,255),2)
+        cv2.imshow("Assigment2", frameCan)
 cap.release()
 cv2.destroyAllWindows()
---
+++ A2_2.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+import time
+
+# Based on tutorial from https://docs.opencv.org/3.4/d9/db0/tutorial_hough_lines.html
+
+frame = cv2.imread("frameTest.jpg")
+
+frame = cv2.resize(frame[:4000, :], (480, 640))
+
+while True:
+
+    ti = time.time()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    cv2.imshow("gray", gray)
+    canny = cv2.Canny(gray, 50, 200)
+
+    cv2.imshow("canny", canny)
+
+    cannyColor = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
+    cannyColorCopy = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
+
+    countourArr = 255*np.ones(gray.shape, np.uint8)
+
+    lines = cv2.HoughLines(canny, 1, np.pi/180, 100)
+
+    if lines is not None:
+        for i in range(0, len(lines)):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+            cv2.line(cannyColor, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.line(countourArr, pt1, pt2, 0, 3, cv2.LINE_AA)
+    
+    linesP = cv2.HoughLinesP(canny, 1, np.pi / 180, 50, None, 50, 10)
+    
+    if linesP is not None:
+        for i in range(0, len(linesP)):
+            l = linesP[i][0]
+            cv2.line(cannyColorCopy, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+    
+
+    contours, q= cv2.findContours(countourArr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    maxArea = 0
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > maxArea:
+            maxArea = area
+            CON = c
+    approxi = cv2.approxPolyDP(CON,0.05*cv2.arcLength(CON,True),True)
+    cv2.drawContours(countourArr, [approxi], -1, (150), 4)
+    cornersOld = np.float32(approxi)
+    cornersNew = np.array([
+        [0, 0],
+        [0, 480],
+        [640, 480],
+        [640, 0],], np.float32)
+
+    if len(cornersOld) == 4:
+
+        getPer = cv2.getPerspectiveTransform(cornersOld, cornersNew)
+        Warped = cv2.warpPerspective(frame, getPer, (640, 480))
+        cv2.imshow("Cowboy and The horse warped", Warped)
+    
+    t = time.time()
+
+    cv2.imshow("onesArr", countourArr)
+    cv2.imshow("Frame", frame)
+    #cv2.imshow("Hough-P", cannyColorCopy)
+    
+    k = cv2.waitKey(10) & 0xFF
+    if k == 27:
+        break
+    
+
